10b40e529df9a0b56522daddecff9d2d3125f3cf
GuillermoPastoriniRivas <guillepastorini@hotmail.com>
2021-08-02 21:57:09 -0300

    tabla suplementos

--- enforma.sql
+++ enforma.sql
@@ -29,3 +29,43 @@ ALTER TABLE `users`
 --
 ALTER TABLE `users`
   MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
+
+  --
+-- Estructura de tabla para la tabla `suplementos`
+--
+
+CREATE TABLE `suplementos` (
+  `id_suplementos` int(11) NOT NULL,
+  `titulo` varchar(200) NOT NULL,
+  `descripcion` varchar(500) NOT NULL,
+  `imagen` varchar(200) NOT NULL,
+  `precio` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `suplementos`
+--
+
+INSERT INTO `suplementos` (`id_suplementos`, `titulo`, `descripcion`, `imagen`, `precio`) VALUES
+(1, 'Whey Protein ENA', 'Suplemento Dietario Whey Protein sabor vainilla en polvo x 1000 gr', 'protein.webp', 3000),
+(2, 'Natural Life Colágeno', 'Suplemento dietario Natural Life Multi collagen x 30 Comprimidos', 'collagen.jpg', 1499);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `suplementos`
+--
+ALTER TABLE `suplementos`
+  ADD PRIMARY KEY (`id_suplementos`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `suplementos`
+--
+ALTER TABLE `suplementos`
+  MODIFY `id_suplementos` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
