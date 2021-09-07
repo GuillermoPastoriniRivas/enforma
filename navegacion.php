3c3d8a83e94cdcf973c31d0bf699850cdf9a4fbe
pedropastorini11 <62403979+pedropastorini11@users.noreply.github.com>
2021-09-07 12:50:15 +0200

    Add files via upload
    menu

---
+++ navegacion.php
@@ -0,0 +1,35 @@
+    <header class="header-area header-sticky <?php if ($data['title']!=='Inicio'):?>background-header<?php endif;?>">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <nav class="main-nav">
+                        
+                        <a href="<?=ROOT?>" class="logo">Enforma<em> Gim</em></a>
+                        
+                        <ul class="nav">
+                            <li class="scroll-to-section"><a href="<?=ROOT?>" 
+                                class="<?php if ($data['title']=='Inicio'):?>active<?php endif;?>">Inicio</a>
+                            </li>
+                            <li class="scroll-to-section"><a href="<?=ROOT?>rutinas" 
+                                class="<?php if ($data['title']=='Rutinas'):?>active<?php endif;?>">Rutinas</a></li>
+                            <li class="scroll-to-section"><a href="<?=ROOT?>suplementos">Suplementos</a></li>
+                          
+                            <li class="scroll-to-section"><a href="<?=ROOT?>tienda"
+                                class="<?php if ($data['title']=='Tienda'):?>active<?php endif;?>">tienda</a></li> 
+                            <li class="main-button"><a href="#">Inscribirse</a></li>
+                            
+                            <li class="scroll-to-section"><a href="<?=ROOT?>contacto"
+                                class="<?php if ($data['title']=='Contacto'):?>active<?php endif;?>">Contacto</a></li> 
+                            <li class="main-button"><a href="#">Inscribirse</a></li>
+
+                        </ul>        
+                        <a class='menu-trigger'>
+                            <span>Menu</span>
+                        </a>
+                        
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    
