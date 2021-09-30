db46850b3a9df9f7bf2efd20a03fbe78f4ee5cd7
pedropastorini11 <62403979+pedropastorini11@users.noreply.github.com>
2021-09-30 14:26:52 -0300

    vestimenta

--- app/views/enforma/partes/navegacion.php
+++ app/views/enforma/partes/navegacion.php
@@ -1,13 +1,9 @@
-  <!-- ***** Header Area Start ***** -->
   <header class="header-area header-sticky <?php if ($data['title']!=='Inicio'):?>background-header<?php endif;?>">
         <div class="container">
             <div class="row">
                 <div class="col-12">
                     <nav class="main-nav">
-                        <!-- ***** Logo Start ***** -->
                         <a href="<?=ROOT?>" class="logo">Enforma<em> Gim</em></a>
-                        <!-- ***** Logo End ***** -->
-                        <!-- ***** Menu Start ***** -->
                         <ul class="nav">
                             <li class="scroll-to-section"><a href="<?=ROOT?>" 
                                 class="<?php if ($data['title']=='Inicio'):?>active<?php endif;?>">Inicio</a>
@@ -18,15 +14,18 @@
                           
                             <li class="scroll-to-section"><a href="<?=ROOT?>contacto"
                                 class="<?php if ($data['title']=='Contacto'):?>active<?php endif;?>">Contacto</a></li> 
+                            <li class="scroll-to-section"><a href="<?=ROOT?>contacto"
+                                class="<?php if ($data['title']=='Contacto'):?>active<?php endif;?>">Contacto</a></li>
+                            
+                                    <li class="scroll-to-section"><a href="<?=ROOT?>vestimenta"
+                                class="<?php if ($data['title']=='Vestimenta'):?>active<?php endif;?>">Vestimenta</a></li> 
                             <li class="main-button"><a href="#">Inscribirse</a></li>
                         </ul>        
                         <a class='menu-trigger'>
                             <span>Menu</span>
                         </a>
-                        <!-- ***** Menu End ***** -->
                     </nav>
                 </div>
             </div>
         </div>
     </header>
-    <!-- ***** Header Area End ***** -->
